e5e6c3f644d467f5e774207144124f290e138a94
Fabian Lallemand <faablallemand@gmail.com>
2018-01-23 13:48:25 +0100

    Initial LED functionalities

---
+++ lamps.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21,GPIO.OUT)
+
+def discolight(finish):
+    if finish = True:
+        GPIO.output(21,True)
+        sleep(5)
+        GPIO.output(21,False)
+        print("joepie")
+    else:
+        print("nog niet klaar of onbekend")
+
+discolight(x)
