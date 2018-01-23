fdd7acfbaae42e485796b4d496c4bfe542a6bc97
Smackerel <35700240+Smackerel@users.noreply.github.com>
2018-01-23 15:29:45 +0100

    Updated sensor.py
    nog niet klaar

--- sensor.py
+++ sensor.py
@@ -1,1 +1,22 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+leftIR = 14
+middleIR = 15
+rightIR = 18
+
 class Sensor:
+
+    def setupsensor():
+
+        print "setting up a sensor"
+        GPIO.setup(leftIR,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(middleIR,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(rightIR,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+
+        GPIO.setup(leftIR,GPIO.IN) #GPIO 14 -> Left IR out
+        GPIO.setup(middleIR,GPIO.IN) #GPIO 15 -> middle IR out
+        GPIO.setup(rightIR,GPIO.IN) #GPIO 18 -> Right IR out
