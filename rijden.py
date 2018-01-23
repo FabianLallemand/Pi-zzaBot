a345d50088288f82a983d607fc14490f0c794ef6
Riezzie <richardkerkvliet@hotmail.com>
2018-01-23 13:57:42 +0100

    button to start

--- rijden.py
+++ rijden.py
@@ -9,31 +9,45 @@ leftIR = 14
 middleIR = 15
 rightIR = 18
 
+buttonPin = 26
+buttonstate = 1
+event = 1
+
 robot = Robot()
 
 GPIO.setup(leftIR,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(middleIR,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 GPIO.setup(rightIR,GPIO.IN,pull_up_down=GPIO.PUD_UP)
 
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 GPIO.setup(leftIR,GPIO.IN) #GPIO 14 -> Left IR out
 GPIO.setup(middleIR,GPIO.IN) #GPIO 15 -> middle IR out
 GPIO.setup(rightIR,GPIO.IN) #GPIO 18 -> Right IR out
 
 while True:
-    curr_left = GPIO.input(leftIR)
-    curr_middle = GPIO.input(middleIR)
-    curr_right = GPIO.input(rightIR)
 
-    if (curr_left == 1) and (curr_middle == 0) and (curr_right == 1):
-        robot.rechtdoor()
-    elif (curr_right != 0) and ((curr_left == 0) or ((curr_left == 0) and (curr_middle == 0))):
-        robot.linksaf()
-    elif (curr_left != 0) and ((curr_right == 0) or ((curr_right == 0) and (curr_middle == 0))):
-        robot.rechtsaf()
-    elif (curr_left == 0 ) and (curr_middle == 0) and (curr_right == 0):
-        print "stopfunctie"
-    else:
-        robot.rechtdoor()
+    buttonstate = GPIO.input(buttonPin)
+
+    if (buttonstate == 0):
+        print "start"
+        while True:
+            curr_left = GPIO.input(leftIR)
+            curr_middle = GPIO.input(middleIR)
+            curr_right = GPIO.input(rightIR)
+
+            if (curr_left == 1) and (curr_middle == 0) and (curr_right == 1):
+                robot.rechtdoor()
+            elif (curr_right != 0) and ((curr_left == 0) or ((curr_left == 0) and (curr_middle == 0))):
+                robot.linksaf()
+            elif (curr_left != 0) and ((curr_right == 0) or ((curr_right == 0) and (curr_middle == 0))):
+                robot.rechtsaf()
+            elif (curr_left == 0 ) and (curr_middle == 0) and (curr_right == 0):
+                print "stopfunctie"
+            else:
+                robot.rechtdoor()
+
+    sleep(.2)
 
 
 GPIO.cleanup()
