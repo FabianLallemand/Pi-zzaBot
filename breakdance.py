990f9bcca82d13d1a93efc7a312e44d9e073e76d
Riezzie <richardkerkvliet@hotmail.com>
2018-01-23 20:07:20 +0100

    race

---
+++ breakdance.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+from time import sleep
+from robot_class import Robot
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+robot = Robot()
+buttonPin = 26
+buttonstate = 1
+started = False
+
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    buttonstate = GPIO.input(buttonPin)
+    if (buttonstate == 0):
+        print "start"
+        started = True
+        while started:
+            print "rijden"
+            robot.scherprechts()
+
+GPIO.cleanup()
