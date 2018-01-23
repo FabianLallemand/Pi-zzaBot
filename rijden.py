ecb76792ce301fedab96724a6ea8ade632be0350
Riezzie <richardkerkvliet@hotmail.com>
2018-01-23 15:23:23 +0100

    conflict resolve

--- rijden.py
+++ rijden.py
@@ -36,12 +36,6 @@ while True:
             curr_left = GPIO.input(leftIR)
             curr_middle = GPIO.input(middleIR)
             curr_right = GPIO.input(rightIR)
-<<<<<<< HEAD
-            if (buttonstate == 0):
-                started = False
-=======
->>>>>>> 8d7b2be95336cdbcbbf9c0257dc2a1ebc13b0eb2
-
             if (curr_left == 1) and (curr_middle == 0) and (curr_right == 1):
                 robot.rechtdoor()
             elif (curr_right != 0) and ((curr_left == 0) or ((curr_left == 0) and (curr_middle == 0))):
