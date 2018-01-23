2849204f451f4cd2ca22908765ae6018ae8120cd
Riezzie <richardkerkvliet@hotmail.com>
2018-01-23 19:53:11 +0100

    race

---
+++ rechtdoor.py
@@ -0,0 +1,18 @@
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+robot = Robot()
+buttonPin = 26
+buttonstate = 1
+started = False
+
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+while True:
+    buttonstate = GPIO.input(buttonPin)
+    if (buttonstate == 0):
+        started = True
+        while started:
+            robot.rechtdoor
+
+GPIO.cleanup()
