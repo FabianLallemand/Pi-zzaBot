7ae36da5bc2dc11f32de5ae4cdc15cbd121dd7bd
Riezzie <richardkerkvliet@hotmail.com>
2018-01-24 13:28:10 +0100

    disco

--- lightemup.py
+++ lightemup.py
@@ -7,6 +7,7 @@ GPIO.setup(21,GPIO.OUT) #linker lichten
 GPIO.setup(20,GPIO.OUT) #worden de disco's
 GPIO.setup(16,GPIO.OUT) #rechter lichten
 GPIO.setup(26,GPIO.OUT)
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 buttonPin = 26
 buttonstate = 1
@@ -24,7 +25,9 @@ while not stop:
             GPIO.output(16,True)
             GPIO.output(20, True)
             sleep(5)
-            started = False
-            stop = False
+            GPIO.output(21,False)
+            GPIO.output(20,False)
+            GPIO.output(16,False)
+            buttonstate = 1
 
 GPIO.cleanup
