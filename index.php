63bd175f1256defc407e5062e0b82d2c771a82f3
Riezzie <richardkerkvliet@hotmail.com>
2018-01-24 16:45:05 +0100

    index verplaats

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+  </head>
+  <body>
+    <center>
+    <div class="container">
+      <div class="col-sm-8" style="background-color:lavender;">
+        </br>
+      <h1>C.R.E.A.M. Remote Control</h1>
+      <p> Kies hieronder uw gewenste afleveradres: </p></br>
+
+
+<form action="doorgave.py">
+  <input type="text" name="route">
+      <!--<div class="btn-group">
+    <button type="button" class="btn btn-primary">Bestemming 1</button>
+    <button type="button" class="btn btn-primary">Bestemming 2</button>
+    <button type="button" class="btn btn-primary">Bestemming 3</button>
+    </div>-->
+  </br></br>
+      <input type="submit" value="Submit" class="btn btn-danger">
+      </br></br>
+    </div>
+  </div>
+</form>
+</center>
+    <!-- jQuery first, then Tether, then Bootstrap JS. -->
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+  </body>
+</html>
