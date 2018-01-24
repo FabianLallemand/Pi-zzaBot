22e29c8ced4f8a887c05164a609a11361a20a69e
Riezzie <richardkerkvliet@hotmail.com>
2018-01-24 10:25:02 +0100

    lights

--- lamps.py
+++ lamps.py
@@ -4,14 +4,14 @@ from time import sleep
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21,GPIO.OUT)
-GPIO.setup(20,GPIO.OUT) #links
+GPIO.setup(20,GPIO.OUT)
 GPIO.setup(16,GPIO.OUT) #rechst
 
 def discolight(finish):
     if finish == True:
-        GPIO.output(20,True)
+        GPIO.output(21,True)
         sleep(5)
-        GPIO.output(20,False)
+        GPIO.output(21,False)
         print("joepie")
     else:
         print("nog niet klaar of onbekend")
