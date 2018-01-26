c606789648aa64d33dcddced7739518cb90e249c
Riezzie <richardkerkvliet@hotmail.com>
2018-01-26 10:13:43 +0100

    clean slate

--- functies_old/rechtdoor.py
+++ functies_old/rechtdoor.py
@@ -1,108 +1,24 @@
 import RPi.GPIO as GPIO
 from time import sleep
+from robot_class import Robot
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
 
-MotorR1 = 2
-MotorR2 = 3
-MotorR3 = 4
-MotorR4 = 17
+robot = Robot()
+buttonPin = 26
+buttonstate = 1
+started = False
 
-MotorL1 = 27
-MotorL2 = 22
-MotorL3 = 10
-MotorL4 = 9
+GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.setup(MotorR1,GPIO.OUT) #GPIO 2  -> Motor rechts A
-GPIO.setup(MotorR2,GPIO.OUT) #GPIO 3  -> Motor rechts B
-GPIO.setup(MotorR3,GPIO.OUT) #GPIO 4  -> Motor rechts C
-GPIO.setup(MotorR4,GPIO.OUT) #GPIO 17 -> Motor rechts D
+while True:
+    buttonstate = GPIO.input(buttonPin)
+    if (buttonstate == 0):
+        print "start"
+        started = True
+        while started:
+            print "rijden"
+            robot.rechtdoor()
 
-GPIO.setup(MotorL1,GPIO.OUT) #GPIO 27 -> Motor Left A
-GPIO.setup(MotorL2,GPIO.OUT) #GPIO 22 -> Motor Left B
-GPIO.setup(MotorL3,GPIO.OUT) #GPIO 10 -> Motor Left C
-GPIO.setup(MotorL4,GPIO.OUT) #GPIO 9  -> Motor Left D
-
-def rechtdoor():
-    print "rechtdoor"
-    GPIO.output(MotorR1,0)
-    GPIO.output(MotorR2,0)
-    GPIO.output(MotorR3,0)
-    GPIO.output(MotorR4,1)
-
-    GPIO.output(MotorL1,0)
-    GPIO.output(MotorL2,0)
-    GPIO.output(MotorL3,0)
-    GPIO.output(MotorL4,1)
-    sleep(.001)
-    GPIO.output(MotorR1,0)
-    GPIO.output(MotorR2,0)
-    GPIO.output(MotorR3,1)
-    GPIO.output(MotorR4,1)
-
-    GPIO.output(MotorL1,0)
-    GPIO.output(MotorL2,0)
-    GPIO.output(MotorL3,1)
-    GPIO.output(MotorL4,1)
-    sleep(.001)
-    GPIO.output(MotorR1,0)
-    GPIO.output(MotorR2,0)
-    GPIO.output(MotorR3,1)
-    GPIO.output(MotorR4,0)
-
-    GPIO.output(MotorL1,0)
-    GPIO.output(MotorL2,0)
-    GPIO.output(MotorL3,1)
-    GPIO.output(MotorL4,0)
-    sleep(.001)
-    GPIO.output(MotorR1,0)
-    GPIO.output(MotorR2,1)
-    GPIO.output(MotorR3,1)
-    GPIO.output(MotorR4,0)
-
-    GPIO.output(MotorL1,0)
-    GPIO.output(MotorL2,1)
-    GPIO.output(MotorL3,1)
-    GPIO.output(MotorL4,0)
-    sleep(.001)
-    GPIO.output(MotorR1,0)
-    GPIO.output(MotorR2,1)
-    GPIO.output(MotorR3,0)
-    GPIO.output(MotorR4,0)
-
-    GPIO.output(MotorL1,0)
-    GPIO.output(MotorL2,1)
-    GPIO.output(MotorL3,0)
-    GPIO.output(MotorL4,0)
-    sleep(.001)
-    GPIO.output(MotorR1,1)
-    GPIO.output(MotorR2,1)
-    GPIO.output(MotorR3,0)
-    GPIO.output(MotorR4,0)
-
-    GPIO.output(MotorL1,1)
-    GPIO.output(MotorL2,1)
-    GPIO.output(MotorL3,0)
-    GPIO.output(MotorL4,0)
-    sleep(.001)
-    GPIO.output(MotorR1,1)
-    GPIO.output(MotorR2,0)
-    GPIO.output(MotorR3,0)
-    GPIO.output(MotorR4,0)
-
-    GPIO.output(MotorL1,1)
-    GPIO.output(MotorL2,0)
-    GPIO.output(MotorL3,0)
-    GPIO.output(MotorL4,0)
-    sleep(.001)
-    GPIO.output(MotorR1,1)
-    GPIO.output(MotorR2,0)
-    GPIO.output(MotorR3,0)
-    GPIO.output(MotorR4,1)
-
-    GPIO.output(MotorL1,1)
-    GPIO.output(MotorL2,0)
-    GPIO.output(MotorL3,0)
-    GPIO.output(MotorL4,1)
-    sleep(.001)
+GPIO.cleanup()
